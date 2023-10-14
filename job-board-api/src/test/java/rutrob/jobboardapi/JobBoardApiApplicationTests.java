d946b876020070a9cdef649654bcf65629ad145d
rutrob <faherlele@gmail.com>
2023-10-14 20:53:30 +0200

    Initial commit

---
+++ job-board-api/src/test/java/rutrob/jobboardapi/JobBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rutrob.jobboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
